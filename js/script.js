47c79088df6a129595789a84572cafd37183da23
Giuseppe <giusepperaffa1988@gmail.com>
2021-01-18 16:37:38 +0100

    Snack 2 , somma pesi

--- js/script.js
+++ js/script.js
@@ -57,13 +57,77 @@
 //Peso = 10
 //Attraverso un prompt dare la possibilità all'utente di modificare il peso della palla.
 
-var oggetto = [
-  {
-  nome : "palla",
-  peso : 10
-  }
-  ];
-for (var i = 0; i < oggetto.length; i++){
-oggetto[i].peso = prompt("inserisci peso");
-}
-console.log(oggetto);
+// var oggetto = [
+//   {
+//   nome : "palla",
+//   peso : 10
+//   }
+//   ];
+// for (var i = 0; i < oggetto.length; i++){
+// oggetto[i].peso = prompt("inserisci peso");
+// }
+// console.log(oggetto);
+
+//***********************************************
+
+//Jsnack2 - Creare un array che contiene 10 oggetti che rappresentano una zucchina, indicandone per ognuno varietà, peso e lunghezza. Calcola quanto pesano tutte le zucchine.
+
+var pugili = [
+    {
+      nome : "R.Marciano",
+      kg : 85,
+      cm :  178
+    },
+    {
+      nome : "J.Louis",
+      kg : 99,
+      cm :  188
+    },
+    {
+      nome : "M.Alì",
+      kg : 97,
+      cm :  191
+    },
+    {
+      nome : "P.Carnera",
+      kg : 110,
+      cm :  197
+    },
+    {
+      nome : "R.Graziano",
+      kg : 72,
+      cm :  170
+    },
+    {
+      nome : "J.LaMotta",
+      kg : 76,
+      cm :  173
+    },
+    {
+      nome : "S.R.Robinson",
+      kg : 78,
+      cm :  180
+    },
+    {
+      nome : "S.R.Leonard",
+      kg : 70,
+      cm :  179
+    },
+    {
+      nome : "M.Tyson",
+      kg : 90,
+      cm :  178
+    },
+    {
+      nome : "G.Foreman",
+      kg : 100,
+      cm :  193
+    }
+    ];
+
+    var totaleKg = 0;
+    for (var i=0; i < pugili.length ; i++){
+    totaleKg += pugili[i].kg;
+    }
+    console.log(pugili);
+    console.log(totaleKg);
